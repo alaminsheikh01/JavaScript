30bb30988799acde1750d113cf5a8d2bc6a67c1f
alaminshiekh01 <alaminsheikh9746@gmail.com>
2022-02-04 02:40:32 +0600

    loop

--- practice.js
+++ practice.js
@@ -1,12 +1,29 @@
-let names = ['Alamin', 'sheikh12', 'Alamin Sheikh'];
-let index = -1;
-let name = names[++index];
+// let names = ['Alamin', 'sheikh12', 'Alamin Sheikh'];
+// let index = -1;
+// let name = names[++index];
 
-setInterval(() =>{
-    name = names[index++]
-    console.log(name, name.length)
+// setInterval(() =>{
+//     name = names[index++]
+//     console.log(name, name.length)
 
-    if(index == names.length){
-        index = 0;
-    }
-}, 1000)
+//     if(index == names.length){
+//         index = 0;
+//     }
+// }, 1000)
+
+// for(let i=1; i<10; i++){
+//     console.log("Hello", i)
+// }
+
+// while(true){
+//     let num = Math.ceil(Math.random() * 100);
+//     console.log("Alamin", num)
+
+//     if(num == 99) {
+//         break;
+//     }
+// }
+
+do{
+    console.log("It wil run at least once")
+}while(false)
