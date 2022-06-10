3dd049bd2382940c8ca192a3acb622349438cb18
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-10 07:55:57 +0600

    work on handleInput

--- Basic React/data-demo/src/App.js
+++ Basic React/data-demo/src/App.js
@@ -1,12 +1,37 @@
-function App() {
+import { useState } from "react";
+
+const initialInputState = {
+  a: 0,
+  b: 0,
+};
+
+const App = () => {
+  const [inputState, setInputState] = useState({ ...initialInputState });
+
+  const handleChangeField = (e) => {
+    setInputState({
+      ...inputState,
+      [e.target.name]: e.target.value,
+      /**
+       * same logic
+       * a: e.target.value
+       * b: e.target.value
+       */
+    });
+  };
   return (
     <div style={{ width: "50%", margin: "0 auto" }}>
       <h1>Result: 0</h1>
 
       <div>
         <p>Inputs</p>
-        <input type="number" />
-        <input type="number" />
+        <input
+          type="number"
+          value={inputState.a}
+          onChange={handleChangeField}
+          name="a"
+        />
+        <input type="number" value={inputState.b} />
       </div>
       <div>
         <p>Operators</p>
@@ -24,6 +49,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
