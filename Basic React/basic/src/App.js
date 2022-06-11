5380942043194938a510d8cfa73f3a333c0f0013
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-11 13:42:01 +0600

    data save as a json from form like data parse from api

--- Basic React/basic/src/App.js
+++ Basic React/basic/src/App.js
@@ -1,14 +1,49 @@
+import { useState } from "react";
+
+const CONTACT_FORM_INIT_STATE = {
+  name: "",
+  email: "",
+};
+
 const ContactForm = () => {
+  const [values, setValue] = useState({ ...CONTACT_FORM_INIT_STATE });
+  const { name, email } = values;
+
+  const handleChange = (e) => {
+    setValue({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email" />
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />
       </div>
+      <br />
       <input type="submit" value="Create New Contact" />
     </form>
   );
