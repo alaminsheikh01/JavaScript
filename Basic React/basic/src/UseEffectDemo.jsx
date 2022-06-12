fa99311b2e5eec747f2f1a5ed717307df8d54906
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-12 10:08:28 +0600

    useEffect basic concept for interview

--- Basic React/basic/src/UseEffectDemo.jsx
+++ Basic React/basic/src/UseEffectDemo.jsx
@@ -5,13 +5,26 @@ const UseEffectDemo = () => {
   const [lock, setLock] = useState(false);
 
   useEffect(() => {
-    const btn = document.getElementById("btn");
-    console.log(btn.offsetHeight, btn.offsetWidth);
-  });
+    if (count === 5) {
+      setLock(true);
+    }
+  }, [count]);
+
+  /**
+   * 1. normal useEffect will do work on componentDidUpdate
+   * 2. when we will give [] after the second parent barcket then will call componentDidMount
+   * 3. if we will give any condition inside the useEffect then it will be shouldComponentUpdate
+   * 4. if we will return something from the useEffect then it will be componentWillUnMount
+   */
+
+  console.log("count: ", count, "lock: ", lock);
+
   return (
     <div>
-      <h1 id="test">Hello world</h1>
-      <button id="btn">click me</button>
+      <h1 id="test">{count}</h1>
+      <button id="btn" disabled={lock} onClick={() => setCount(count + 1)}>
+        ADD {lock && "(locked)"}
+      </button>
       <div id="container"></div>
     </div>
   );
