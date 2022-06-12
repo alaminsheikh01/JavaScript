1ea6561e06f3946a592ff686342573d86a3056e9
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-13 00:46:54 +0600

    best way to fetch the user with useEffect

--- Basic React/basic/src/Work_api.jsx
+++ Basic React/basic/src/Work_api.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const Work_api = () => {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  const [user, setUser] = useState([]);
+
+  // fetch data way one; but here is a problem inside the network tab to load data infinite.
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => res.json())
+  //     .then((data) => setUsers(data));
+
+  // fetch data way two.
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/2")
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+
   return (
     <div>
-      <h1>API</h1>
+      <h1>Users Details</h1>
+      {user && (
+        <div>
+          name: {user.name}
+          <br />
+          email: {user.email}
+          <br />
+          phone: {user.phone}
+        </div>
+      )}
     </div>
   );
 };
