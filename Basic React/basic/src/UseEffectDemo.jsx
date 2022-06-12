a2747e5ef4d0874d2470dffcf65bf04a0d0ba454
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-13 00:25:41 +0600

    useEffect count app done

--- Basic React/basic/src/UseEffectDemo.jsx
+++ Basic React/basic/src/UseEffectDemo.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 const UseEffectDemo = () => {
   const [count, setCount] = useState(0);
   const [lock, setLock] = useState(false);
+  const [timeCont, setTimeCount] = useState(5);
 
   useEffect(() => {
     if (count === 5) {
@@ -16,24 +17,29 @@ const UseEffectDemo = () => {
    * 3. if we will give any condition inside the useEffect then it will be shouldComponentUpdate
    * 4. if we will return something from the useEffect then it will be componentWillUnMount
    */
+  let timeInterval = null;
   useEffect(() => {
-    let time = null;
-    if (lock) {
-      time = setTimeout(() => {
-        setCount(0);
-        setLock(false);
-        clearTimeout(time);
-      }, 5000);
+    if (lock && timeInterval === null) {
+      timeInterval = setInterval(() => {
+        setTimeCount((prev) => prev - 1);
+      }, 1000);
     }
   }, [lock]);
 
-  console.log("count: ", count, "lock: ", lock);
+  useEffect(() => {
+    if (timeCont === 0) {
+      clearInterval(timeInterval);
+      setCount(0);
+      setLock(false);
+      setTimeCount(5);
+    }
+  }, [timeCont]);
 
   return (
     <div>
       <h1 id="test">{count}</h1>
       <button id="btn" disabled={lock} onClick={() => setCount(count + 1)}>
-        ADD {lock && "(locked)"}
+        ADD {lock && `(locked - ${timeCont}s)`}
       </button>
       <div id="container"></div>
     </div>
