7443d7beec6e33bd542f8998b0eaf9fca364a801
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-12 08:47:37 +0600

    find the button height weight

--- Basic React/basic/src/UseEffectDemo.jsx
+++ Basic React/basic/src/UseEffectDemo.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const UseEffectDemo = () => {
+  const [count, setCount] = useState(0);
+  const [lock, setLock] = useState(false);
+
+  useEffect(() => {
+    const btn = document.getElementById("btn");
+    console.log(btn.offsetHeight, btn.offsetWidth);
+  });
   return (
     <div>
-      <h1>App</h1>
+      <h1 id="test">Hello world</h1>
+      <button id="btn">click me</button>
+      <div id="container"></div>
     </div>
   );
 };
