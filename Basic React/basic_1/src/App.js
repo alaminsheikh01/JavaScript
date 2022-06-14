1b2bde114605947ebb2cf39c3abbe94256155306
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-14 09:02:35 +0600

    make a custom hook for a counter app

--- Basic React/basic_1/src/App.js
+++ Basic React/basic_1/src/App.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import useCounter from "./hooks/useCounter";
+
 function App() {
+  // const [counter1, setCounter1] = useState(0);
+  const { count, handleInc, handleDec } = useCounter();
+  const count1 = useCounter();
+
+  const CounterController = ({ count, handleInc, handleDec }) => {
+    return (
+      <div>
+        <button onClick={handleInc}>+</button>
+        <span>{count}</span>
+        <button onClick={handleDec}>-</button>
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div>
+      <h1>App</h1>
+      <CounterController
+        count={count}
+        handleInc={handleInc}
+        handleDec={handleDec}
+      />
+      <CounterController
+        count={count1.count}
+        handleInc={count1.handleInc}
+        handleDec={count1.handleDec}
+      />
     </div>
   );
 }
---
+++ Basic React/basic_1/src/hooks/useCounter.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const useCounter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleInc = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  };
+  const handleDec = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+  return {
+    count,
+    handleDec,
+    handleInc,
+  };
+};
+
+export default useCounter;
