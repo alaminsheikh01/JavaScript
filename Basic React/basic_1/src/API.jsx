37d87a5673248c19a30e5927479799f1721dcb53
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-14 09:45:28 +0600

    working with API

---
+++ Basic React/basic_1/src/API.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function API() {
+  return (
+    <div style={{ width: "600px", display: "flex", gap: "1rem" }}>
+      <div>
+        <h1>Users</h1>
+      </div>
+      <div>
+        <h1>Posts</h1>
+      </div>
+    </div>
+  );
+}
+
+export default API;
