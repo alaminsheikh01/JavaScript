146ffd12ccff4f932b32f7ff034d1ca6084ba5ff
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-14 21:25:46 +0600

    write sample css using styled component

--- Basic React/basic_1/src/StyleComponent.jsx
+++ Basic React/basic_1/src/StyleComponent.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import styled from "styled-components";
+
+const BaseButton = styled.button`
+  border: none;
+  outline: none;
+  background: black;
+  color: white;
+  font-size: 0.9rem;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+`;
 
 function StyleComponent() {
   return (
     <div>
       <h1>Styled Component </h1>
+      <BaseButton>I am a button</BaseButton>
     </div>
   );
 }
