994382361d20cf6d8c3fd46fd1bc6ab7df14c889
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-14 21:55:07 +0600

    responsive fontSize

--- Basic React/basic_1/src/StyleComponent.jsx
+++ Basic React/basic_1/src/StyleComponent.jsx
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+const fontSize = {
+  sm: "0.8rem",
+  md: "1rem",
+  lg: "1.2rem",
+};
+
 const BaseButton = styled.button`
   border: none;
   outline: none;
@@ -10,13 +16,23 @@ const BaseButton = styled.button`
   letter-spacing: 2px;
   padding: 0.5rem 1rem;
   cursor: pointer;
+  border-radius: 0.15rem;
+  font-size: ${(props) => fontSize[props.size] ?? "1rem"};
+`;
+
+const PrimaryButton = styled(BaseButton)`
+  background: red;
+  color: white;
 `;
 
 function StyleComponent() {
   return (
     <div>
       <h1>Styled Component </h1>
-      <BaseButton dark>I am a button</BaseButton>
+      <BaseButton dark size="sm">
+        I am a button
+      </BaseButton>
+      <PrimaryButton size="sm">Primary Button</PrimaryButton>
     </div>
   );
 }
