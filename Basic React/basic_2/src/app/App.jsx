2c61f96c992da0f6694816d3ddb5165ffc1de421
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-06-17 09:11:54 +0600

    form validity

--- Basic React/basic_2/src/app/App.jsx
+++ Basic React/basic_2/src/app/App.jsx
@@ -1,16 +1,79 @@
+import { useState } from "react";
+import InputGroup from "../components/shared/forms/InputGroup";
 import Button from "../components/UI/button/Button";
-import TextInput from "../components/UI/inputs/TextInput";
-import Text from "../components/UI/texts/Text";
+const init = {
+  title: "",
+  bio: "",
+  skills: "",
+};
 
 function App() {
+  const [formState, setFormState] = useState({ ...init });
+  const [errors, setErrors] = useState({ ...init });
+
+  const handleChange = (e) => {
+    setFormState((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { isValid, errors } = checkValidity(formState);
+    if (isValid) {
+      console.log(formState);
+    } else {
+      console.log(errors);
+    }
+  };
+
+  const checkValidity = (formState) => {
+    const errors = {};
+    const { title, bio, skills } = formState;
+    if (!title) {
+      errors.title = "Invali title";
+    }
+    if (!bio) {
+      errors.bio = "invalid bio";
+    }
+    if (!skills) {
+      errors.skills = "invalid skills";
+    }
+
+    return {
+      errors,
+      isValid: Object.keys(errors).length === 0,
+    };
+  };
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <TextInput />
-      <Button>Test Me</Button>
-      <Text size="md" line="lg">
-        Hello Styled component
-      </Text>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          <InputGroup
+            onChange={handleChange}
+            value={formState.title}
+            label={"Title"}
+            name={"title"}
+            placeholder={"Software Engineer"}
+          />
+          <InputGroup
+            onChange={handleChange}
+            value={formState.bio}
+            label={"Bio"}
+            name={"bio"}
+            placeholder={"I am a software engineer..."}
+          />
+          <InputGroup
+            onChange={handleChange}
+            value={formState.skills}
+            label={"Skills"}
+            name={"skills"}
+            placeholder={"javascript, react"}
+          />
+          <Button type="submit">Submit</Button>
+        </div>
+      </form>
     </div>
   );
 }
