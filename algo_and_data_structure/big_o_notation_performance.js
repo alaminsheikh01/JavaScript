c5c7a91f9cb4184bfb9f00dd2f4538703a18cdfd
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-07-03 18:14:00 +0600

    added complexity

--- algo_and_data_structure/big_o_notation_performance.js
+++ algo_and_data_structure/big_o_notation_performance.js
@@ -1,4 +1,4 @@
-//  way one
+//  way one // big 0(n)
 function sumOfAll(n) {
   let total = 0;
   for (let i = 0; i <= n; i++) {
@@ -9,12 +9,22 @@ function sumOfAll(n) {
 
 // console.log(sumOfAll(10));
 
-// way two // best case for performance
+// way two // best case for performance // big 0(1)
 function sumOfAll(n) {
   return (n * (n + 1)) / 2;
 }
-let time1 = performance.now();
-console.log(sumOfAll(5));
-let time2 = performance.now();
+// let time1 = performance.now();
+// console.log(sumOfAll(5));
+// let time2 = performance.now();
 
-console.log(`your time ${time1 - time2} / 1000`);
+// console.log(`your time ${time1 - time2} / 1000`);
+
+// big 0(n)2
+function multipleArray(n) {
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= n; j++) {
+      console.log(i, j);
+    }
+  }
+}
+console.log(multipleArray(2));
