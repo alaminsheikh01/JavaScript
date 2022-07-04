dc79e24269a2a94f9fae6cbfd532a51e9be303d3
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-07-05 01:40:36 +0600

    big o notaion analyize

---
+++ algo_and_data_structure/analyze_big_o.js
@@ -0,0 +1,8 @@
+const myObject = {
+  channel: "coding",
+  instructor: "alamin sheikh",
+  videos: [4, 5, 3, 2],
+};
+console.log(Object.keys(myObject));
+console.log(Object.values(myObject));
+console.log(Object.entries(myObject));
---
+++ algo_and_data_structure/problem_solve_approce.js
@@ -0,0 +1,8 @@
+/**
+ * Maintain 4 cncepts
+ *
+ * 1. understant the problem
+ * 2. break it down
+ * 3. solve and simplify
+ * 4. look back and refactor
+ */
