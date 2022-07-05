bd4bbcb4885afc9174963da3d0361d9461ec4e68
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-07-05 09:19:28 +0600

    charCount solved

---
+++ algo_and_data_structure/charCount.js
@@ -0,0 +1,15 @@
+function charCount(str) {
+  let charList = {};
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase();
+
+    if (charList[char] > 0) {
+      charList[char]++;
+    } else {
+      charList[char] = 1;
+    }
+  }
+  return charList;
+}
+
+console.log(charCount("hello Everyone"));
