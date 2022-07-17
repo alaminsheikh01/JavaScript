00151b96c6e5497272bad9c2fe872a350a64e020
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-07-17 16:29:59 +0600

    singly linked list done with push and pop

--- algo_and_data_structure/linked_list/SinglyLinkedList.js
+++ algo_and_data_structure/linked_list/SinglyLinkedList.js
@@ -10,7 +10,7 @@ class SinglyLinkedList {
     return this.length === 0;
   }
 
-  // push value
+  // push method
   push(value) {
     let newNode = {
       value: value,
@@ -25,6 +25,36 @@ class SinglyLinkedList {
     }
     this.length++;
   }
+
+  // pop method
+  pop() {
+    if (!this.head) {
+      return null;
+    }
+    if (this.length === 1) {
+      let removeNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      return removeNode;
+    }
+
+    let currentNode = this.head;
+    let lastNode = this.tail;
+    let newLastNode;
+
+    while (currentNode) {
+      if (currentNode.next === this.tail) {
+        newLastNode = currentNode;
+        break;
+      }
+      currentNode = currentNode.next;
+    }
+    newLastNode.next = null;
+    this.tail = newLastNode;
+    this.length--;
+    return lastNode;
+  }
 }
 
 let list = new SinglyLinkedList();
@@ -32,5 +62,9 @@ let list = new SinglyLinkedList();
 // console.log(list.isEmpty());
 list.push(5);
 list.push(8);
+list.push(10);
+list.push(15);
+
+list.pop();
 // console.log(list.isEmpty());
 console.log(list);
