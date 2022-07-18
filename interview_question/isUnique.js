4b80cc9328e8d6830f69c0b101c8ae5d2812e48e
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-07-19 01:35:42 +0600

    isUnique string check

---
+++ interview_question/isUnique.js
@@ -0,0 +1,16 @@
+function IsUnique(str) {
+  let obj = {};
+
+  for (let i of str) {
+    !obj[i] ? (obj[i] = 1) : obj[i]++;
+  }
+
+  for (let i in obj) {
+    if (obj[i] > 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(IsUnique("kunle".toLowerCase()));
