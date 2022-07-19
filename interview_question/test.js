c933efdf9521aea086319258ebd92f0501a265be
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-07-19 16:49:46 +0600

    Track zone project starte

---
+++ interview_question/test.js
@@ -0,0 +1,11 @@
+/**
+ * check permutaion
+ * Given two strings, write a method to decide if one is a permutaion of the other.
+ *
+ * algorithm
+ *
+ * 1. check if they are the same exact strings - else
+ * 2. check the lengths of both string - else
+ * 3. create objects character count are equal - else
+ * 4. return true
+ */
