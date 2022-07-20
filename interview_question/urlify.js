5c21cdf1cfa55b24d04c5a3558603c8e9e38a999
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-07-20 16:02:01 +0600

    URLify problem statement

---
+++ interview_question/urlify.js
@@ -0,0 +1,16 @@
+/**
+ * write a method to replace all spaces in a string with "%20"
+ * Assume that there is sufficient space at the end.
+ *
+ * input: ("Mr Alamin sheikh ", 13)
+ * output: Mr%20Alamin%20sheikh
+ *
+ * algorithm: o(n)
+ * loop through the string until you encounter white space
+ * replace with '%20'
+ *
+ * algorithm
+ * check to see if string last element is white space
+ * convert string to array
+ * join with '%20'
+ */
