fffa12daa9bf776218c773354fdfb71aac2aeee0
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-08-18 00:05:30 +0600

    MUI button

---
+++ mui/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Button variant="contained">Test button</Button>
+    </>
+  );
+};
+
+export default App;
